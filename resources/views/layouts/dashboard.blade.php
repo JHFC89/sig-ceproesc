f8731667f6f02cb28d9d6a0d60156772e02219eb
JHFC89 <faber.calarga@gmail.com>
2020-11-06 12:03:15 -0300

    Creates dashboard layout view component

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="api-base-url" content="{{ url('api') }}" />
+    <title>@yield('title') - SIG Ceproesc</title>
+    <link rel="stylesheet" href="{{ mix('css/main.css') }}">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
+</head>
+<body class="flex h-screen overflow-hidden">
+
+    <x-side-navbar/>
+
+    <main class="w-full h-full overflow-y-auto bg-gray-200">
+
+        <nav class="w-full h-16 bg-white shadow"></nav>
+
+        <div class="px-10 py-10 space-y-8">
+            @yield('content')
+        </div>
+
+    </main>
+
+    @section('scripts')
+        <script type="text/javascript" src="{{ mix('js/app.js') }}" defer></script>
+    @show
+</body>
+</html>
