ba45dd8d54c8afc5e38afa46210fb61ceb73882c
JHFC89 <faber.calarga@gmail.com>
2020-11-06 12:02:29 -0300

    Creates side-navbar view component

---
+++ resources/views/components/side-navbar.blade.php
@@ -0,0 +1,15 @@
+<aside class="w-64 h-full overflow-y-auto text-gray-100 bg-gray-700">
+    <div class="flex items-center h-16 px-4 text-lg uppercase bg-gray-900 shadow"> 
+        <span class="font-serif text-3xl font-medium text-white">sig</span><span class="ml-2">- ceproesc</span>
+    </div>
+    <nav class="px-2 py-4">
+        <ul class="text-gray-100">
+            <li>
+                <a href="#" class="flex items-center px-2 py-2 group hover:bg-gray-800 rounded-md">
+                    <x-icons.register-lesson class="w-6 text-gray-300 group-hover:text-gray-200"/>
+                    <span class="ml-4 group-hover:text-white">Registro de Aulas</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
