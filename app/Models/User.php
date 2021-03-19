bd5e45b5c4f1b4c22518ba61e50d61b5d7d1b426
JHFC89 <faber.calarga@gmail.com>
2021-03-19 11:47:13 -0300

    Refactors User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, UserTestData;
 
     /**
      * The attributes that are mass assignable.
---
+++ app/Models/UserTestData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+trait UserTestData
+{
+    public static function fakeCoordinator()
+    {
+        return static::factory()->hasRoles(['name' => 'coordinator'])->create();
+    }
+
+    public static function fakeInstructor()
+    {
+        return static::factory()->hasRoles(['name' => 'instructor'])->create();
+    }
+
+    public static function fakeNovice()
+    {
+        return static::factory()->hasRoles(['name' => 'novice'])->create();
+    }
+
+    public static function fakeEmployer()
+    {
+        return static::factory()->hasRoles(['name' => 'employer'])->create();
+    }
+}
